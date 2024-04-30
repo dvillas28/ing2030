a2767b17d0c670caba9b8d9df20faf0e94707c54
dvillas28 <dvillasc@gmail.com>
2024-04-30 00:28:50 -0400

    added data_paths

---
+++ backend/data_paths.py
@@ -0,0 +1,8 @@
+from os import path
+
+CERRO_NAVIA = path.join('backend', 'data', 'cerro_navia.csv')
+CONSUMO_ELECTRICO = path.join('backend', 'data', 'consumo_electrico.csv')
+
+if __name__ == '__main__':
+    print(CERRO_NAVIA)
+    print(CONSUMO_ELECTRICO)

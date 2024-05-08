d5d82c0af1e9c4e72bdb4bc0e2bb944634af2d50
dvillas28 <dvillasc@gmail.com>
2024-05-08 00:49:03 -0400

    agregados resultados del calculo de datos

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template
 from backend import utils
 from backend import data_paths
+from backend import resultados_opti
 
 app = Flask(__name__)
 
@@ -11,6 +12,10 @@ cn_data = utils.get_generacion_electrico(data_paths.CERRO_NAVIA)
 consumo_data = utils.get_consumo_electrico(path=data_paths.CONSUMO_ELECTRICO,
                                            path2=data_paths.CERRO_NAVIA)
 
+# results from the optimization model
+problema, x, z, I = resultados_opti.obtener_resultados(
+    path=data_paths.RESULTADOS)
+
 
 @app.route('/')
 def index():
@@ -20,7 +25,7 @@ def index():
 @app.route('/dia_<int:i>')
 def mostrar_grafico(i):
     if i == 0 or i == 366:
-        return render_template('index.html')
+        return render_template('home.html')
 
     data_of_the_day = utils.get_day_data(i, cn_data)
 
@@ -41,6 +46,24 @@ def mostrar_grafico(i):
                            fecha=data_of_the_day["fecha"],
                            dataset_cn=dataset_cn,
                            dataset_consumo=dataset_consumo)
+
+
+@app.route('/ahorro_dia_<int:i>')
+def mostrar_ahorros(i):
+    x_of_the_day = utils.get_day_data_of_results(i, x)
+    z_of_the_day = utils.get_day_data_of_results(i, z)
+    I_of_the_day = utils.get_day_data_of_results(i, I)
+
+    print('| d | h |  x  |  z  |  I  |')
+    print('|---|---|-----|-----|-----|')
+    for t in range(1, 25):
+        print(
+            f'| {i} | {t} | {round(x_of_the_day[t], 2)} | {round(z_of_the_day[t], 2)} | {round(I_of_the_day[t], 2)} |')
+
+    return render_template('ahorro.html',
+                           x=x_of_the_day,
+                           z=z_of_the_day,
+                           I=I_of_the_day)
 
 
 if __name__ == '__main__':
